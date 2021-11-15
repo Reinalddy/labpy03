138e391b074e6642ae6d55741e9cd09990cc20f3
reinalddy <reinalddyaldy183@gmail.com>
2021-11-15 21:05:55 +0700

    menambahkan folder

---
+++ program perulangan mencari bilangan terbesar/latihan 2.py
@@ -0,0 +1,38 @@
+
+(print (""""
+
+    nama :reinalddy
+    nim :312110522
+    
+
+
+"""))
+
+
+start = 10
+stop = 0 
+step = 100
+
+for i in list(range(stop,start,step)):
+    a = int (input("masukan angka  ="))
+    b = int (input("masukan angka  ="))
+
+    if a < b :
+        print ("bilangan terbesar adalah =",b)
+
+    elif a > b :
+        print ("bilangan terbesar adalah :",a)    
+
+   
+    
+          
+
+
+print ("selesai")  
+
+
+
+    
+    
+
+  
